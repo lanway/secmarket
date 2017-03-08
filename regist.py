ae99ea86e976b6965034096c2d5b0ba37ead87bf
lanway <115564098@qq.com>
2017-03-08 20:13:19 +0800

    db updata

--- regist.py
+++ regist.py
@@ -25,43 +25,53 @@ class regist(BaseHandler):
         utel = self.get_argument('phone')
         ucollege = self.get_argument('college')
         try:
-            exist = self.db.query(User).filter(or_(User.Utel == utel,User.Ucardnum == ucardnum ,User.Ualais == ualais)).one()
+            exist = self.db.query(User).filter(User.Utel == utel).one()
             self.retjson['code'] = '10000'
-            self.retjson['contents'] = '用户已注册'
+            self.retjson['contents'] = '用户电话已注册'
         except Exception,e:
-            print 'ccc'
-            upaw = md5(upaw)
-            user = User(
-                Upassword=upaw,
-                Utel = utel,
-                Ualais = ualais,
-                Usex = usex,
-                Ucollege = ucollege,
-                Ucardnum = ucardnum,
-                Ustunum = '123',
-            )
             try:
-                print 'aaa'
-                self.db.merge(user)
-                print 'bbb'
-                self.db.commit()
-                print '111'
-                m_id = self.db.query(User.Uid).filter(User.Utel == utel).one()
-                print '222'
-                userimage = UserImage(
-                    UIuid = m_id[0],
-                    UIurl = "user-default-image.jpg",
-                )
-                print '333'
-                self.db.merge(userimage)
-                print '444'
-                self.db.commit()
-                print '555'
-                self.retjson['code'] = '10001'
-                self.retjson['contents'] = '注册成功'
-            except Exception,e:
-                print 'sdddd'
-                print e
-                self.retjson['code'] = '10009'
-                self.retjson['contents'] = '服务器错误'
+                exist = self.db.query(User).filter(User.Ucardnum== ucardnum).one()
+                self.retjson['code'] = '10002'
+                self.retjson['contents'] = '用户一卡通已注册'
+            except Exception, e:
+                try:
+                    exist = self.db.query(User).filter(User.Ualais == ualais).one()
+                    self.retjson['code'] = '10003'
+                    self.retjson['contents'] = '用户昵称已注册'
+                except Exception, e:
+                    print 'ccc'
+                    upaw = md5(upaw)
+                    user = User(
+                        Upassword=upaw,
+                        Utel = utel,
+                        Ualais = ualais,
+                        Usex = usex,
+                        Ucollege = ucollege,
+                        Ucardnum = ucardnum,
+                        Ustunum = '123',
+                    )
+                    try:
+                        print 'aaa'
+                        self.db.merge(user)
+                        print 'bbb'
+                        self.db.commit()
+                        print '111'
+                        m_id = self.db.query(User.Uid).filter(User.Utel == utel).one()
+                        print '222'
+                        userimage = UserImage(
+                            UIuid = m_id[0],
+                            UIurl = "user-default-image.jpg",
+                        )
+                        print '333'
+                        self.db.merge(userimage)
+                        print '444'
+                        self.db.commit()
+                        print '555'
+                        self.retjson['code'] = '10001'
+                        self.retjson['contents'] = '注册成功'
+                    except Exception,e:
+                        print 'sdddd'
+                        print e
+                        self.retjson['code'] = '10009'
+                        self.retjson['contents'] = '服务器错误'
         self.write(json.dumps(self.retjson,ensure_ascii=False,indent=2))
