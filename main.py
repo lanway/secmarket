02c0a09a9db5c4d4d85bc369504522cc95bdc6fe
lanway <115564098@qq.com>
2017-03-24 22:58:47 +0800

    user类

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from tornado.options import define, options
 from Activity.Trendlist import Trendlist
 from Activity.createAC import createAC
 from Database.models import engine
-from User.Userdetail import userdetail
+from User.Userdetails import userdetail
 from login import Login
 from regist import regist
 
